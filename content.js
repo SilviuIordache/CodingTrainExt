de303d2e9f4dd06b5500236c9f58ce8147f878f1
Silviu <silviu.iordache92@gmail.com>
2019-08-27 19:02:56 +0300

    added background script that listens to btn cliks

--- content.js
+++ content.js
@@ -1,5 +1,23 @@
 console.log('Chrome Extension go!');
-let paragrahps = document.getElementsByTagName('p');
-for (el of paragrahps) {
-  el.style['background-color'] = '#FF00FF';
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+  console.log(message.txt);
+
+  if (message.txt === "hello") {
+    let paragrahps = document.getElementsByTagName('p');
+    for (el of paragrahps) {
+      el.style['background-color'] = getRandomColor();
+    }
+  }
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
