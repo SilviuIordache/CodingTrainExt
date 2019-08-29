0ed99056f6d848e534d145a73ec4e1b90f180410
Silviu <silviu.iordache92@gmail.com>
2019-08-29 17:49:12 +0300

    Started work on doodler extension

---
+++ Doodler/sketch.js
@@ -0,0 +1,12 @@
+console.log('sketch');
+
+function setup() {
+  console.log('sketch started!');
+  createCanvas(400, 400);
+  // c.position(0, 0);
+}
+
+function draw() {
+  background(0);
+  console.log('sketch looping!');
+}
